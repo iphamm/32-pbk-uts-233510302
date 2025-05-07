9a77fe60b44527e9b3d032521060b1f5f2ddc503
M. Ilham Ramadhana <milhamramadhana25@gmail.com>
2025-05-07 08:51:04 +0700

    Menambahkan fitur menampilkan to-do list menggunakan v-for

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <script setup>
+import { ref } from 'vue'
 
+const todos = ref([
+  { text: 'Cardio'},
+  { text: 'Watch Netflix Movie'},
+])
 </script>
 
 <template>
   <div>
     <h1>To-Do List</h1>
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">
+        {{ todo.text }}
+      </li>
+    </ul>
   </div>
 </template>
 
